5ba1e90be37da767d100338884f184fbd80dc12b
Reza <homayunir@gmail.com>
2024-08-20 11:59:37 -0400

    Created dashboardControllers, Router, and Redux Query for dashboard metrics

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import helmet from "helmet";
 import morgan from "morgan";
 // ROUTE IMPORTS
-
+import dashboardRoutes from "./routes/dashboardRoutes";
 
 // CONFIGURATIONS
 dotenv.config();
@@ -19,10 +19,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 // ROUTES
-app.get("/hello", (req, res) => {
-    res.send("hello world");
-});
-
+app.use("/dashboard", dashboardRoutes); // http://localhost:8000/dashboard
 
 // SERVER
 const port = process.env.PORT || 3001;
