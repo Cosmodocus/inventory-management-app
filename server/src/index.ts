d8377a39fb2f3ca7100c7b05c743b81e4aae18ce
Reza <homayunir@gmail.com>
2024-09-17 13:51:39 -0400

    ec2 configs added

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,5 +26,5 @@ app.use("/products", productRoutes); // http://localhost:8000/products
 app.use("/users", userRoutes); // http://localhost:8000/users
 
 // SERVER
-const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const port = Number(process.env.PORT )|| 3001;
+app.listen(port, "0.0.0.0", () => console.log(`Server running on port ${port}`));
