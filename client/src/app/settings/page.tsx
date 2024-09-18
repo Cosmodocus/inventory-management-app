b7f185c81c09ffb2985f25b2aeba0a859ee6e527
Reza <homayunir@gmail.com>
2024-09-18 12:30:48 -0400

    Light/dark mode connected to settings page toggle switch

--- client/src/app/settings/page.tsx
+++ client/src/app/settings/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import {useState} from "react";
-import Header from "../(components)/Header";
+import { useState } from "react";
+import Header from "@/app/(components)/Header";
+import { useAppDispatch, useAppSelector } from "../redux";
+import { setIsDarkMode } from "@/state";
 
 type UserSetting = {
   label: string;
@@ -9,20 +11,31 @@ type UserSetting = {
 };
 
 const mockSettings: UserSetting[] = [
-  {label: "Username", value: "john_doe", type: "text"},
-  {label: "Email", value: "john.doe@example.com", type: "text"},
-  {label: "Notification", value: true, type: "toggle"},
-  {label: "Dark Mode", value: false, type: "toggle"},
-  {label: "Language", value: "English", type: "text"},
+  { label: "Username", value: "john_doe", type: "text" },
+  { label: "Email", value: "john.doe@example.com", type: "text" },
+  { label: "Notification", value: true, type: "toggle" },
+  { label: "Dark Mode", value: false, type: "toggle" },
+  { label: "Language", value: "English", type: "text" },
 ];
 
 const Settings = () => {
   const [userSettings, setUserSettings] = useState<UserSetting[]>(mockSettings);
+  const dispatch = useAppDispatch();
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
 
   const handleToggleChange = (index: number) => {
     const settingsCopy = [...userSettings];
-    settingsCopy[index].value = !settingsCopy[index].value as boolean;
+    const updatedValue = !settingsCopy[index].value as boolean;
+    settingsCopy[index].value = updatedValue;
     setUserSettings(settingsCopy);
+
+    if (settingsCopy[index].label === "Dark Mode") {
+      toggleDarkMode();
+    }
   };
 
   return (
@@ -32,20 +45,13 @@ const Settings = () => {
         <table className="min-w-full bg-white rounded-lg">
           <thead className="bg-gray-800 text-white">
             <tr>
-              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
-                Setting
-              </th>
-              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">
-                Value
-              </th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Setting</th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Value</th>
             </tr>
           </thead>
           <tbody>
             {userSettings.map((setting, index) => (
-              <tr
-                className="hover:bg-blue-50"
-                key={setting.label}
-              >
+              <tr className="hover:bg-blue-50" key={setting.label}>
                 <td className="py-2 px-4">{setting.label}</td>
                 <td className="py-2 px-4">
                   {setting.type === "toggle" ? (
